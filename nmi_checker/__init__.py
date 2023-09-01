a4863fc7693f46c8fe6299195516d595a278acea
Akhlaq <akhlaq.computer@gmil.com>
2023-09-01 18:59:20 +0800

    Readme updated, class name imported in __init__.py

---
+++ nmi_checker/__init__.py
@@ -0,0 +1,1 @@
+from nmi_checker import RangeChecker
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="nmi-checker",
-    version="1.2.0",
+    version="1.4.0",
     packages=find_packages(),
     package_data={
         'nmi_checker': ['separated_data.json']
