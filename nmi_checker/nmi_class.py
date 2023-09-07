395d4cb7e684ac5bd1d1aaf89e646f73e50eb186
Alfie Chadwick <75963032+Fonzzy1@users.noreply.github.com>
2023-09-07 16:43:17 +1000

    nmi_class use ord instead of int

--- nmi_checker/nmi_class.py
+++ nmi_checker/nmi_class.py
@@ -128,7 +128,7 @@ class NmiChecker:
         result, found = self.find_special_string(text)
         if found:
             generated_digit = self.generate(result)
-            last_character = int(result[-1])
+            last_character = ord(result[-1])
             if len(result) > 10 and generated_digit == last_character:
                 print(f"Checksum passed for {result[:10]}")
                 return result[:10], True
